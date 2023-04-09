60521f57a2866bf0da1a42ce4a70de0ffcce4402
Xiechen Zheng <zhengxiechen@gmail.com>
2023-04-09 10:29:39 -0400

    add slash command for monthly reminder

--- app.js
+++ app.js
@@ -8,10 +8,34 @@ const app = new App({
   appToken: process.env.SLACK_APP_TOKEN
 });
 
-// Listens to incoming messages that contain "hello"
-app.message('hello', async ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
-  await say(`Hey there <@${message.user}>!`);
+var cron = require("cron");
+const channelId = process.env.CHANNEL_ID;
+
+function getMonth() {
+  var currentdate = new Date(); 
+  curMonth = currentdate.getMonth()
+  return curMonth
+}
+
+function sendCelebrationByMonth() {
+  const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+  ];
+  monthCeleMessage = 'Birthday stars for ' + `${monthNames[getMonth()]} ` + 'are ... ' + '\n';
+  app.client.chat.postMessage({
+    channel: channelId,
+    text: monthCeleMessage
+  });
+}
+
+app.command("/birthday", async ({ ack }) => {
+  try {
+    await ack()
+    sendCelebrationByMonth();
+  } catch (error) {
+      console.log("err")
+    console.error(error);
+  }
 });
 
 (async () => {
@@ -19,4 +43,16 @@ app.message('hello', async ({ message, say }) => {
   // Start your app
   await app.start(process.env.PORT || port);
   console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
+  
+  const date = '10 10 * * *'
+  let scheduledMessage = new cron.CronJob(date, () => {
+    // This runs every day at 10:30:00, you can do anything you want
+        app.client.chat.postMessage({
+          channel: channelId,
+          text: `custom cron job date`
+        });
+      });
+          
+      // When you want to start it, use:
+  scheduledMessage.start()
 })();
