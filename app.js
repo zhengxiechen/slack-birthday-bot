01e6d89f3f4a58b3891cc8248e7586d10bc83230
Xiechen Zheng <zhengxiechen@gmail.com>
2023-04-09 13:12:45 -0400

    added task to check birthday each day

--- app.js
+++ app.js
@@ -4,29 +4,34 @@ var app = lib.app;
 
 app.command("/birthday", async ({ ack }) => {
   try {
-    await ack()
-    lib.sendCelebrationByMonth()
+    await ack();
+    lib.sendCelebrationByMonth();
   } catch (error) {
-      console.log("err")
+      console.log("err");
     console.error(error);
   }
 });
 
 (async () => {
-  const port = 3000
+  const port = 3000;
   // Start your app
   await app.start(process.env.PORT || port);
   console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
 
-  const date = '10 10 * * *'
-  let scheduledMessage = new cron.CronJob(date, () => {
-    // This runs every day at 10:30:00, you can do anything you want
-        app.client.chat.postMessage({
-          channel: process.env.CHANNEL_ID,
-          text: `custom cron job date`
-        });
-      });
-          
-      // When you want to start it, use:
-  scheduledMessage.start()
+  const firstDayofMonth = '0 8 1 * *';
+  let scheduledFirstDayofMonthMessage = new cron.CronJob(firstDayofMonth, () => {
+    // This runs on first day of each month at 8 am to check 
+    // whether there are anyone having birthday in the month
+      lib.sendCelebrationByMonth();
+  });
+  scheduledFirstDayofMonthMessage.start();
+
+  const eachDay = '0 8 * * *';
+  let scheduledEachDayMessage = new cron.CronJob(eachDay, () => {
+    // This runs on each day at 8 am to check 
+    // whether there is anyone having the birthday today
+      lib.sendCelebrationByDay();
+  });
+  scheduledEachDayMessage.start();
+
 })();
